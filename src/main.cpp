88f33d55f407649acc60cee51c188a7a0a94300b
Xtreme Prime <XtremePrimeDev@gmail.com>
2015-12-11 23:36:54 +0200

    Created project

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <SFML/Audio.hpp>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello!\n";
+    return EXIT_SUCCESS;
+}
